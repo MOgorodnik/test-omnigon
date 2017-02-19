1c2ea58a1a1913a39b2ba74864bf380606217b14
MOgorodnik <mr.ogorodnik@gmail.com>
2017-02-20 00:42:08 +0200

    Commit sidebar

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,21 @@
-console.log('This is the Main.js file. It should be the 3rd and final file');
+//console.log('This is the Main.js file. It should be the 3rd and final file');
+
+(function() {
+
+	"use strict";
+
+	var toggles = document.querySelectorAll(".menu-btn-switcher");
+
+	for (var i = toggles.length - 1; i >= 0; i--) {
+		var toggle = toggles[i];
+		toggleHandler(toggle);
+	};
+
+	function toggleHandler(toggle) {
+		toggle.addEventListener( "click", function(e) {
+			e.preventDefault();
+			(this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+		});
+	}
+
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var runSequence = require('run-sequence');
 // Basic Gulp task syntax
 gulp.task('hello', function() {
   console.log('Hello Zell!');
-})
+});
 
 // Development Tasks 
 // -----------------
@@ -27,23 +27,26 @@ gulp.task('browserSync', function() {
       baseDir: 'app'
     }
   })
-})
+});
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'IE 11']
+    }))
     .pipe(gulp.dest('app/css')) // Outputs it in the css folder
     .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
     }));
-})
+});
 
 // Watchers
 gulp.task('watch', function() {
   gulp.watch('app/scss/**/*.scss', ['sass']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
-})
+});
 
 // Optimization Tasks 
 // ------------------
@@ -72,17 +75,17 @@ gulp.task('images', function() {
 gulp.task('fonts', function() {
   return gulp.src('app/fonts/**/*')
     .pipe(gulp.dest('fonts'))
-})
+});
 
 // Cleaning 
 gulp.task('clean', function() {
   return del.sync('./').then(function(cb) {
     return cache.clearAll(cb);
   });
-})
+});
 
 gulp.task('clean:dist', function() {
-  return del.sync(['**/*', '!images', '!images/**/*', '!indexb.html', '!.git', '!.git/**/*', '!app', '!app/**/*', '!node_modules', '!node_modules/**/*', '!.gitignore', '!gulpfile.js', '!package.json', '!README.md', '!css', '!css/**/*', '!js', '!js/**/*']);
+  return del.sync(['**/*', '!images', '!images/**/*', '!indexb.html', '!.git', '!.git/**/*', '!app', '!app/**/*', '!node_modules', '!node_modules/**/*', '!.gitignore', '!gulpfile.js', '!package.json', '!README.md', '!css', '!css/**/*', '!js', '!js/**/*', '!.idea', '!.idea/**/*', '!info.txt']);
 });
 
 // Build Sequences
@@ -92,7 +95,7 @@ gulp.task('default', function(callback) {
   runSequence(['sass', 'browserSync'], 'watch',
     callback
   )
-})
+});
 
 gulp.task('build', function(callback) {
   runSequence(
@@ -101,4 +104,4 @@ gulp.task('build', function(callback) {
     ['useref', 'images', 'fonts'],
     callback
   )
-})
+});
